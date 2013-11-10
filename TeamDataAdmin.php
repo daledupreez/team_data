90aca02c6145e5ea6b2130d1bcc35dc03bc706d4
Dale du Preez <daledupreez@gmail.com>
2013-11-09 23:56:34 -0500

    Fix to paging with season filter

--- TeamDataAdmin.php
+++ TeamDataAdmin.php
@@ -742,7 +742,7 @@ jQuery('.team_data_select_image_button').live('click', function( event ){
 			echo "team_data.paging.resultCount = $matchCount;";
 			echo '</script>';
 
-			$season_where = ($season_id <= 0 ? '' : "AND m.season_id = $season_id");
+			$season_where = ($season_id === 0 ? '' : "AND m.season_id = $season_id");
 			$match_query = "SELECT m.id, m.season_id, CONCAT(s.year,' ',s.season) AS season, m.date, DATE_FORMAT(m.date,'%M %d, %Y') AS pretty_date, TIME_FORMAT(m.time,'%h:%i %p') AS time, m.venue_id, v.name AS venue_name, m.opposition_id, m.opposition_name, m.tourney_name, m.level_id, l.name AS level_name, m.our_score, m.opposition_score, m.result, m.is_league, m.is_postseason
 				FROM 
 					( SELECT match.id, match.season_id, match.date, match.time, match.venue_id, match.opposition_id, match.tourney_name, match.level_id, match.our_score, match.opposition_score, match.result, match.is_league, match.is_postseason, IF(match.opposition_id IS NULL, '', team.name) AS opposition_name
@@ -753,7 +753,6 @@ jQuery('.team_data_select_image_button').live('click', function( event ){
 			$this->debug('$match_query = ' . $match_query);
 
 			$matches = $wpdb->get_results($match_query);
-			$matchCount = count($matches);
 			if (count($matches) > 0) {
 				$back_disabled = ($pageNum == 0 ? 'disabled="disabled" ' : '');
 				$next_disabled = ($pageNum == $maxPage ? 'disabled="disabled" ' : '');
